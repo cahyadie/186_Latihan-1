dbcbefca95f754ecb81c41abf71ad2a3c7725741
cahyadie <cahyadiilham12@gmail.com>
2023-03-06 12:01:38 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // begin
+    // numericluas,p,l
+    double luas, p, l;
+    // display 'masukan panjangnya ='
+    cout << "masukan panjangnya = ";
+    // accept p
+    cin >> p;
+    // display 'masukan lebarnya = '
+    cout << "masukan lebarnya = ";
+    // accept l;
+    cin >> l;
+    // compute luas = p*l
+    luas = p*l;
+    // display "luasnya = "+luas
+    cout << "luasnya = " << luas;
+    // end
+}
