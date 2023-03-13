74aba392f4e9e2f7a4419d6bad2e04b03269c827
cahyadie <cahyadiilham12@gmail.com>
2023-03-13 11:26:28 +0700

    tambah kondisi

---
+++ Pertemuan3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a,double b){
+    return (a+b)/2;
+}
+
+string status (double c){
+    if (c >= 60 )
+        return "Lulus";
+    else 
+        return "Gagal";
+}
+
+int main (){
+    double nilM,nilB;
+    cout << "masukan nilai matematika = ";
+    cin >> nilM;
+    cout << "masukan nilai bahasa indonesia = ";
+    cin >> nilB;
+    cout << "status kelulusan = " << status(rerata(nilM,nilB));
+    return 0;
+}
